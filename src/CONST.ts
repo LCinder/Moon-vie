9e7a57cebd61d8bf8e6a0d35d7ea2ca1bf995494
diego <diegohm1598@gmail.com>
2021-10-10 18:29:16 +0200

    Cambiadas URLS

--- src/CONST.ts
+++ src/CONST.ts
@@ -1,6 +1,12 @@
 
 export enum CONST {
-    TMDB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=",
-    TMDB_SEARCH_LANGUAGE_QUERY = "&language=es-ES&query=",
-    TMDB_PAGE = "&page=1" //&include_adult=false"
+    TMDB_BASIC_URL = "https://api.themoviedb.org/3/",
+    TMDB_SEARCH_URL = "search/movie",
+    TMDB_API = "?api_key=",
+    TMDB_MOVIE_URL = "movie/",
+    TMDB_REVIEWS_URL = "reviews",
+    TMDB_LANGUAGE_ES = "&language=es-ES",
+    TMDB_LANGUAGE_EN = "&language=en-US",
+    TMDB_QUERY = "&query=",
+    TMDB_PAGE = "&page=1" //&include_adult=false",
 }
