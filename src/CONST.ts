ef76355f4511bde844c32f9a614fa5d78bf07b6b
diego <diegohm1598@gmail.com>
2021-10-10 17:31:37 +0200

    Incluido archivo CONST con constantes closes #12

---
+++ src/CONST.ts
@@ -0,0 +1,6 @@
+
+export enum CONST {
+    TMDB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=",
+    TMDB_SEARCH_LANGUAGE_QUERY = "&language=es-ES&query=",
+    TMDB_PAGE = "&page=1" //&include_adult=false"
+}
