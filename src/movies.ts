492bd7319edb4df620b6dcd6f7677cfd78a985d0
diego <diegohm1598@gmail.com>
2021-10-15 11:29:18 +0200

    MV #5 closes #17 : Creada clase Movies que carga las peliculas desde un JSON

---
+++ src/movies.ts
@@ -0,0 +1,33 @@
+import movies from "../data/movies.json";
+import {Movie} from "./movie"
+
+
+export class Movies {
+    private _movies: any;
+
+    constructor() {
+        this._movies = movies;
+    }
+
+    find(title: string): any {
+        let res: any = this._movies.movies.find((e: { original_title: string; }) => {
+            if(e.original_title === title) {
+                res = e;
+                console.log("res1")
+                return e;
+            }
+        });
+        return res;
+    }
+
+    convertJSON2Movie(json: any): Movie {
+        const movie = new Movie();
+        movie.id = json.id;
+        movie.overview = json.overview;
+        movie.popularity = json.popularity;
+        movie.voteAverage = json.vote_average;
+        json.reviews.forEach((review: string) => movie.addReview(review));
+        return movie;
+    }
+
+}
