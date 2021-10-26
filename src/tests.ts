dfcf42ec6860b96d7fbb2caf224bb4eeaf375981
diego <diegohm1598@gmail.com>
2021-10-26 13:05:29 +0200

    MV #20 #2 : Incluido vote average es numero y en rango entre 0 y 10

--- src/tests.ts
+++ src/tests.ts
@@ -25,7 +25,15 @@ describe("Quiero obtener informacion de una pelicula", () => {
     it("Reviews deberia ser un array y no estar vacio", () => {
         assert.isArray(movie.reviews);
         assert.isNotEmpty(movie.reviews);
-        console.log(movie.toString());
-
     });
+    it("Vote average deberia ser un nº entre 0 y 10", () => {
+        assert.isNumber(movie.voteAverage);
+        assert.isAtLeast(movie.voteAverage, 0);
+        assert.isAtMost(movie.voteAverage, 10);
+    });
+/*    it("Spy", () => {
+        const spy = sinon.spy(Movie.prototype, "addReview");
+        const movie2 = movies.find("Tenet");
+        assert.equal(spy.callCount, 5);
+    });*/
 });
