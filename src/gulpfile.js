d772d8c958e40df58b646fd50521672cf97fdde8
LCinder <diegohm1598@gmail.com>
2021-10-27 19:48:35 +0200

    MV #21 #5 : Cambiada tarea compilar y test e incluido tsconfig

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,11 +1,16 @@
 
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
+const mocha = require("gulp-mocha");
+const tsProject = ts.createProject("tsconfing.json");
 
 gulp.task("default", () => {
     return gulp.src("./*.ts")
-    .pipe(ts({
-        isolatedModules: true
-    }))
-    .pipe(gulp.dest("./"));
+    .pipe(ts())
+    .pipe(gulp.dest("./"))
+    .pipe(tsProject());
+});
+
+gulp.task("test", () => {
+   gulp.src("./tests.js").pipe(mocha());
 });
