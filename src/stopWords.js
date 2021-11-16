27a5a4f8c9c4d866a83437249a4dd2116645c91e
diego <diegohm1598@gmail.com>
2021-11-16 13:31:10 +0100

    Incluyendo stopWords.js

---
+++ src/stopWords.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.stopWords = void 0;
+var stopWords = ["movie", "movies", "film", "films"];
+exports.stopWords = stopWords;
