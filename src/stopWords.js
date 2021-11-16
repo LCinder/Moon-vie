9eba5452b4aea9021312686dda2a7174eafb949e
diego <diegohm1598@gmail.com>
2021-11-16 14:36:19 +0100

    Quitando archivo stopWords.js transpilado

--- src/stopWords.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.stopWords = void 0;
-var stopWords = ["movie", "movies", "film", "films"];
-exports.stopWords = stopWords;
