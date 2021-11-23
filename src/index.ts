a216c8248e952bc2648ef6755768ee516bb9f07e
diego <diegohm1598@gmail.com>
2021-11-23 10:30:53 +0100

    MV #33 : Quitando modulos no usados e incluyendo interfaz peliculas

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 
 import {Movie} from "./movie"
 import {Movies} from "./movies"
-import lda from "lda";
-//import rake from "rake-js";
+import {IMovie} from "./IMovie";
 
 function test(title: string): Movie {
     const movies: Movies = new Movies();
-    const json: any = movies.find(title);
+    const json: IMovie = movies.find(title);
     const movie: Movie = new Movie(movies.convertJSON2Movie(json));
     return movie;
 }
 
 const movie = test("The Hobbit: An Unexpected Journey");
-let keywords: string[] = movie.extractKeywords();//movie.overview
+const keywords: string[] = movie.extractKeywords();
 console.log(keywords);
