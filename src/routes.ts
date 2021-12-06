0bd2e5e603fbd9f4247ce3eb068678c579d002aa
diego <diegohm1598@gmail.com>
2021-12-06 17:39:11 +0100

    MV #6 : Incluyendo logger y logs infor y error

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import {Movie} from "./movie" ;
 import {Movies} from "./movies";
 import fastify, {FastifyRequest} from "fastify";
 
-export const server = fastify();
+export const server = fastify({logger: true});
 /**********************************************************************************************************************/
 /**********************************************************************************************************************/
 server.get("/status", async (request, reply) => {
@@ -12,8 +12,14 @@ server.get("/status", async (request, reply) => {
 /**********************************************************************************************************************/
 /**********************************************************************************************************************/
 server.get("/movies", async (request, reply) => {
-    const movies = new Movies();
-    reply.code(200).send(movies.movies);
+    try {
+        const movies = new Movies();
+        request.log.info("Movies sent");
+        reply.code(200).send(movies.movies);
+    } catch(err: any) {
+        request.log.error(err.message);
+        reply.code(400).send({error: err.message, code: 400});
+    }
 });
 /**********************************************************************************************************************/
 /**********************************************************************************************************************/
@@ -21,8 +27,10 @@ server.get("/movies/:movie", async (request: any, reply) => {
     try {
         const movies = new Movies();
         const movie: Movie = new Movie(movies.convertJSON2Movie(movies.find(request.params.movie)));
+        request.log.info(`Movie ${movie.title} sent`);
         reply.code(200).send(JSON.stringify(movie));
     } catch(err: any) {
+        request.log.error(`${err.message} : ${request.params.movie}`);
         reply.code(404).send({error: err.message, code: 404});
     }
 });
@@ -33,11 +41,13 @@ server.get("/movies/:movie/keywords", async (request: any, reply) => {
         const movies = new Movies();
         const movie: Movie = new Movie(movies.convertJSON2Movie(movies.find(request.params.movie)));
         const keywords: string[] = movie.extractKeywords();
+        request.log.info(`Extracted and sent keywords from movie ${movie.title}`);
         reply.code(200).send(JSON.stringify(keywords));
     } catch(err: any) {
-        reply.code(404).send({error: err.message, code: 404});
+        request.log.error(`${err.message} : ${request.params.movie}`);
+        reply.code(404).send(JSON.stringify({error: err.message, code: 404}));
     }
 });
 /**********************************************************************************************************************/
 /**********************************************************************************************************************/
-//server.listen(9000)
+server.listen(5001)
